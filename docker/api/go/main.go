a2b345fd69e55eaf2e8241d183b90e11b18764a1
RyoichiNakai <elmobasketball0913@me.com>
2021-02-05 14:49:54 +0900

    add db

--- docker/api/go/main.go
+++ docker/api/go/main.go
@@ -4,43 +4,18 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
+	"github.com/gin-gonic/gin" // ginフレームワーク
+	_ "github.com/go-sql-driver/mysql" // mysql用ドライバー
+	"github.com/jinzhu/gorm" // gorm
 	"github.com/joho/godotenv"
 )
-
-func main() {
-	// 環境変数ファイルの読み込み
-	err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	DBMigrate(DBConnect())
-
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "dadsa",
-		})
-	})
-
-	r.Run(":8080")
-}
 
 type User struct {
 	gorm.Model
 	NickName string `json:"nickName"`
 }
 
-func DBMigrate(db *gorm.DB) *gorm.DB {
-	db.AutoMigrate(&User{})
-	return db
-}
-
-func DBConnect() *gorm.DB {
+func gormConnect() *gorm.DB {
 	DBMS := "mysql"
 	USER := os.Getenv("MYSQL_USER")
 	PASS := os.Getenv("MYSQL_PASSWORD")
@@ -52,4 +27,58 @@ func DBConnect() *gorm.DB {
 		panic(err.Error())
 	}
 	return db
+}
+
+func dbInit() {
+	db := gormConnect()
+	defer db.Close() // コネクション解放
+
+	db.AutoMigrate(&User{})
+}
+
+func dbInsert(nickname string) {
+	db := gormConnect()
+	defer db.Close()
+
+	db.Create(&User{NickName: nickname})
+}
+
+func dbGetAll() []User {
+	db := gormConnect()
+	defer db.Close()
+
+	var users []User
+	// 取得した情報は引数で与えたモデルに格納される
+	db.Find(&users)
+	return users
+}
+
+func main() {
+	// 環境変数ファイルの読み込み
+	err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "dadsadasdsa",
+		})
+	})
+
+	router.GET("/users", func (c *gin.Context)  {
+		users := dbGetAll()
+		c.JSON(200, gin.H{
+			"users": users,
+		})
+	})
+
+	router.POST("/users/:nickname", func (c *gin.Context)  {
+		nickname := c.Param("nickname")
+		dbInsert(nickname)
+	})
+
+	router.Run(":8080")
 }
